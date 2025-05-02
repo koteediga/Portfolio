0bdb28c017b7b9af0a6903a090a365934cdc45d3
koteswararao <koteediga85@gmail.com>
2025-05-02 16:17:55 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,139 @@
+const logotext = "KOTESWARARAO";
+const meta = {
+    title: "Ediga Koteswararao",
+    description: "I’m Koteswararao, a Full-Stack Developer skilled in React.js and Node.js.",
+};
+
+const introdata = {
+    title: "I’m Ediga Koteswararao",
+    animated: {
+        first: "I love coding",
+        second: "I build full-stack web apps",
+        third: "I learn and adapt fast",
+    },
+    description:
+        "Aspiring full-stack developer with strong skills in React.js, Node.js, and MongoDB. Passionate about building scalable web applications and solving real-world problems through code.",
+    your_img_url: "https://avatars.githubusercontent.com/u/139219703?v=4", 
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme:
+        "I am a full-stack developer with hands-on experience in creating dynamic, responsive, and secure web applications. I’m currently pursuing an Integrated M.Tech in CSE from VIT Amaravati and have completed an Industry-Ready Certification from Nxtwave. I love working on challenging projects and constantly improving my skills.",
+};
+
+const worktimeline = [
+    {
+        jobtitle: "Top 5 - V-Launchpad Entrepreneurship Program",
+        where: "State Level",
+        date: "2024",
+    },
+    {
+        jobtitle: "Team Captain - Volleyball Nationals",
+        where: "India",
+        date: "2023",
+    },
+];
+
+const skills = [
+    { name: "React.js", value: 85 },
+    { name: "Node.js", value: 85 },
+    { name: "JavaScript", value: 90 },
+    { name: "MongoDB", value: 70 },
+    { name: "Python", value: 80 },
+    { name: "Java", value: 75 },
+    { name: "HTML/CSS/Bootstrap", value: 90 },
+];
+
+const services = [
+    {
+        title: "Full-Stack Web Development",
+        description:
+            "Creating robust web applications using MERN stack, with authentication, routing, and RESTful APIs.",
+    },
+    {
+        title: "Backend API Development",
+        description:
+            "Developing scalable and secure REST APIs using Node.js, Express.js, and MongoDB with JWT authentication.",
+    },
+    {
+        title: "Frontend Design with React",
+        description:
+            "Building responsive UIs with modern React components, Bootstrap, and API integration.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://res.cloudinary.com/dh8jgl2ue/image/upload/v1746181042/clipfly-ai-20250502153338_xjmtmy.jpg",
+        description: "Jobby App - A full-featured job search platform built with React and JWT authentication.",
+        link: "https://github.com/koteediga/jobbyApp",
+    },
+    {
+        img: "https://as2.ftcdn.net/jpg/10/60/45/27/1000_F_1060452720_vJx5ZrLdibiAXSiVHlmj4UE5SKKjLIU3.jpg",
+        description:
+            "Yoga Master API - A secure REST backend for a yoga learning platform with class management, JWT auth, and payments.",
+        link: "https://github.com/koteediga/Yoga-Master-Server",
+    },
+    {
+    img: "https://res.cloudinary.com/dh8jgl2ue/image/upload/v1746182685/lt1nl8zeftwbxfxw8rux.jpg",
+    description:
+        "Medicure Frontend - A responsive React.js interface for booking doctor appointments and managing prescriptions, with seamless API integration for real-time healthcare data updates.",
+    link: "https://github.com/koteediga/Medicure",
+},{
+    img: "https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png",
+    description:
+        "Nxt Trendz – A secure e-commerce clone inspired by Amazon & Flipkart with JWT authentication and dynamic product pages.",
+    link: "https://github.com/koteediga/TrendZauthentication"
+}
+,{
+    img: "https://community.fabric.microsoft.com/t5/image/serverpage/image-id/915954i05068C40F94AC588?v=v2",
+    description:
+        "IPL Dashboard – A React app to explore IPL teams and matches with dynamic routing and graceful 404 handling.",
+    link: "https://github.com/koteediga/CompleteIPLdahboard"
+}
+,{
+    img: "https://api.covid19india.org/wp-content/uploads/2024/09/Covid-19-API-India-2024-State-Wise.jpg", 
+    description:
+        "COVID-19 India Portal - A secure RESTful API built with Express.js and SQLite to manage state-wise and district-wise COVID stats with JWT-based authentication and role-based access.",
+    link: "https://github.com/koteediga/Covid19IndiaPortal",
+},
+
+{
+    img: "https://res.cloudinary.com/dh8jgl2ue/image/upload/v1746182029/ogdv4oea6wvzxvuuxoc9.jpg", 
+    description:
+        "Emoji Game - A fun and interactive React.js memory game where players must click unique emojis without repeating, testing attention and memory skills.",
+    link: "https://github.com/koteediga/EmojiGame",
+}
+
+,
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "koteediga85@gmail.com",
+    YOUR_FONE: "9618507312",
+    description:
+        "If you're interested in collaborating or hiring, feel free to reach out. I’m open to exciting opportunities in full-stack development.",
+    YOUR_SERVICE_ID: "your_emailjs_service_id",
+    YOUR_TEMPLATE_ID: "your_emailjs_template_id",
+    YOUR_USER_ID: "your_emailjs_user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/koteediga",
+    linkedin: "https://www.linkedin.com/in/ediga-koteswararao-905749234/",
+    leetcode: "https://leetcode.com/u/m1DkMQiZ3D/",
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
